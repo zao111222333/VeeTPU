04d3e94df04bb00d6fe86072f59e2ae7f4565723
zao111222333 <zhou@junzhuo.me>
2022-02-01 17:57:50 +0800

    new version

--- src/main/scala/veetpu/reconfigurable/vector/MULExpCompare.scala
+++ src/main/scala/veetpu/reconfigurable/vector/MULExpCompare.scala
@@ -49,6 +49,7 @@ class MULExpCompareBasic() extends Component{
   io.o.Mode1(0) := max(1) @@ max(0)
 }
 
+
 class MULExpCompareDiff() extends Component{
   val ctl = new Bundle{
     val i = new Bundle{
