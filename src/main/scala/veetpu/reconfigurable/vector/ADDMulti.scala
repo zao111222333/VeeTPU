6ac26d996a135ac7405c1acbfea1750f4f3f5fcb
zao111222333 <zhou@junzhuo.me>
2022-02-06 23:35:55 +0800

    new version

--- src/main/scala/veetpu/reconfigurable/vector/ADDMulti.scala
+++ src/main/scala/veetpu/reconfigurable/vector/ADDMulti.scala
@@ -12,40 +12,30 @@ import spinal.lib._
 ** --------------------------------   |_| \_\   |_|   |_____|   -------------------------------- **
 \* --------------------------------                             -------------------------------- */
 
+class ADDBasic16() extends Component{
+  val io = new Bundle{
+    val i = new Bundle{
+      val carry = in Vec(UInt( 1 bits), 16)
+      val input = in Vec(UInt(23 bits), 16)
+    }
+    val o = new Bundle{
+      val sum = out UInt(23+4+1 bits)
+    }
+  }
+    io.o.sum := ((((io.i.input( 0)+^io.i.input( 1))+^(io.i.input( 2)+^io.i.input( 3)))+^
+                  ((io.i.input( 4)+^io.i.input( 5))+^(io.i.input( 6)+^io.i.input( 7))))+^
+                 (((io.i.input( 8)+^io.i.input( 9))+^(io.i.input(10)+^io.i.input(11)))+^
+                  ((io.i.input(12)+^io.i.input(13))+^(io.i.input(14)+^io.i.input(15)))))
+  noIoPrefix()
+}
+
 class ADDMulti() extends Component{
-  def sum(in0:SInt,in1:SInt):SInt={
-    return in0+^in1
-  }
-  def sum(in:Array[SInt]):SInt={
-    val len = in.length
-    var maxWidth = in(0).getWidth
-    val sumArray = Array.tabulate(len-1)((n) => {
-      maxWidth = maxWidth+1
-      maxWidth = maxWidth.max(in(n+1).getWidth)
-      SInt(maxWidth bits)
-    })
-    sumArray(0) := sum(in(0),in(1))
-    Array.tabulate(len-2)((n) => {
-      sumArray(n+1) := sum(sumArray(n),in(n+2))
-    })
-    sumArray(len-2)
-  }
-  def sum(in:Vec[SInt]):SInt={
-    val _in = Array.tabulate(in.length)((n) => { in(n) })
-    sum(_in)
-  }
-  def sum(in1:Vec[SInt],in2:Vec[SInt]):SInt={
-    val _in = Array.tabulate(in1.length+in2.length)((n) => { 
-      if(n>in1.length-1) in2(n-in1.length) else in1(n)
-    })
-    sum(_in)
-  }
   val ctl = new Bundle{
     val i = new Bundle{
       val mode = in (Mode())
     }
   }
-
+  def oneComplement(trueForm: UInt, enable: Bool): UInt = enable.asUInt @@ Mux(enable, ~trueForm, trueForm)
   val io = new Bundle{
     val i = new Bundle{
       val mant = new Bundle{
@@ -112,84 +102,93 @@ class ADDMulti() extends Component{
     }
   }
   noIoPrefix()
-  val sInt = new Bundle{
+  val OneComplement = new Bundle{
     val mul = new Bundle{
-      val INT8 = Vec(Vec(SInt(  8*2+1 bits), 16), 4)
-      val BF16 = Vec(Vec(SInt(  8*2+1 bits), 16), 4)
-      val FP16 = Vec(Vec(SInt( 11*2+1 bits), 16), 4)
-      val FP32 = Vec(Vec(SInt( 24*2+1 bits),  8), 2)
-      val TF32 = Vec(Vec(SInt( 11*2+1 bits),  8), 2)
-      val FP64 = Vec(Vec(SInt( 53*2+1 bits),  4), 1)
+      val INT8 = Vec(Vec(UInt(  8*2+1 bits), 16), 4)
+      val BF16 = Vec(Vec(UInt(  8*2+1 bits), 16), 4)
+      val FP16 = Vec(Vec(UInt( 11*2+1 bits), 16), 4)
+      val FP32 = Vec(Vec(UInt( 24*2+1 bits),  8), 2)
+      val TF32 = Vec(Vec(UInt( 11*2+1 bits),  8), 2)
+      val FP64 = Vec(Vec(UInt( 53*2+1 bits),  4), 1)
     }
     val mac = new Bundle{
-      val INT8 = Vec(Vec(SInt(   32 bits),  1), 4)
-      val BF16 = Vec(Vec(SInt(  8+1 bits),  1), 4)
-      val FP16 = Vec(Vec(SInt( 11+1 bits),  1), 4)
-      val FP32 = Vec(Vec(SInt( 24+1 bits),  1), 2)
-      val TF32 = Vec(Vec(SInt( 11+1 bits),  1), 2)
-      val FP64 = Vec(Vec(SInt( 53+1 bits),  1), 1)
+      val INT8 = Vec(Vec(UInt(   32 bits),  1), 4)
+      val BF16 = Vec(Vec(UInt(  8+1 bits),  1), 4)
+      val FP16 = Vec(Vec(UInt( 11+1 bits),  1), 4)
+      val FP32 = Vec(Vec(UInt( 24+1 bits),  1), 2)
+      val TF32 = Vec(Vec(UInt( 11+1 bits),  1), 2)
+      val FP64 = Vec(Vec(UInt( 53+1 bits),  1), 1)
     }
     val add = new Bundle{
-      val INT8 = Vec(Vec(SInt(   32 bits),  1), 4)
-      val BF16 = Vec(Vec(SInt(  8+1 bits),  1), 4)
-      val FP16 = Vec(Vec(SInt( 11+1 bits),  1), 4)
-      val FP32 = Vec(Vec(SInt( 24+1 bits),  1), 2)
-      val TF32 = Vec(Vec(SInt( 11+1 bits),  1), 2)
-      val FP64 = Vec(Vec(SInt( 53+1 bits),  1), 1)
+      val INT8 = Vec(Vec(UInt(   32 bits),  1), 4)
+      val BF16 = Vec(Vec(UInt(  8+1 bits),  1), 4)
+      val FP16 = Vec(Vec(UInt( 11+1 bits),  1), 4)
+      val FP32 = Vec(Vec(UInt( 24+1 bits),  1), 2)
+      val TF32 = Vec(Vec(UInt( 11+1 bits),  1), 2)
+      val FP64 = Vec(Vec(UInt( 53+1 bits),  1), 1)
     }
   }
   Array.tabulate(4)((m) => {
     Array.tabulate(16)((n) => {
-      sInt.mul.INT8(m)(n) := io.i.mant.mul.INT8(m)(n)
+      OneComplement.mul.INT8(m)(n) := io.i.mant.mul.INT8(m)(n).asUInt
     })
-    sInt.mac.INT8(m)(0) := io.i.mant.mac.INT8(m)(0)
-    sInt.add.INT8(m)(0) := io.i.mant.add.INT8(m)(0)
-    io.o.INT8(m) :=(ctl.i.mode.isMUL? sum(sInt.mul.INT8(m)) |
-                   (ctl.i.mode.isMAC? sum(sInt.mul.INT8(m),sInt.mac.INT8(m))| 
-                   (ctl.i.mode.isMAC? sum(sInt.add.INT8(m),sInt.mac.INT8(m))| S"0".resized))).resized
+    OneComplement.mac.INT8(m)(0) := io.i.mant.mac.INT8(m)(0).asUInt
+    OneComplement.add.INT8(m)(0) := io.i.mant.add.INT8(m)(0).asUInt
   })
   Array.tabulate(4)((m) => {
     Array.tabulate(16)((n) => {
-      sInt.mul.BF16(m)(n) := io.i.mant.mul.BF16(m)(n).twoComplement(io.i.sign.mul.BF16(m)(n))
-      sInt.mul.FP16(m)(n) := io.i.mant.mul.FP16(m)(n).twoComplement(io.i.sign.mul.FP16(m)(n))
+      OneComplement.mul.BF16(m)(n) := oneComplement(io.i.mant.mul.BF16(m)(n),io.i.sign.mul.BF16(m)(n))
+      OneComplement.mul.FP16(m)(n) := oneComplement(io.i.mant.mul.FP16(m)(n),io.i.sign.mul.FP16(m)(n))
     })
-    sInt.mac.BF16(m)(0) := io.i.mant.mac.BF16(m)(0).twoComplement(io.i.sign.mac.BF16(m)(0))
-    sInt.add.BF16(m)(0) := io.i.mant.add.BF16(m)(0).twoComplement(io.i.sign.add.BF16(m)(0))
-    sInt.mac.FP16(m)(0) := io.i.mant.mac.FP16(m)(0).twoComplement(io.i.sign.mac.FP16(m)(0))
-    sInt.add.FP16(m)(0) := io.i.mant.add.FP16(m)(0).twoComplement(io.i.sign.add.FP16(m)(0))
-    io.o.BF16(m) :=(ctl.i.mode.isMUL? sum(sInt.mul.BF16(m)) |
-                   (ctl.i.mode.isMAC? sum(sInt.mul.BF16(m),sInt.mac.BF16(m))| 
-                   (ctl.i.mode.isMAC? sum(sInt.add.BF16(m),sInt.mac.BF16(m))| S"0".resized))).resized
-    io.o.FP16(m) :=(ctl.i.mode.isMUL? sum(sInt.mul.FP16(m)) |
-                   (ctl.i.mode.isMAC? sum(sInt.mul.FP16(m),sInt.mac.FP16(m))| 
-                   (ctl.i.mode.isMAC? sum(sInt.add.FP16(m),sInt.mac.FP16(m))| S"0".resized))).resized
+    OneComplement.mac.BF16(m)(0) := oneComplement(io.i.mant.mac.BF16(m)(0),io.i.sign.mac.BF16(m)(0))
+    OneComplement.add.BF16(m)(0) := oneComplement(io.i.mant.add.BF16(m)(0),io.i.sign.add.BF16(m)(0))
+    OneComplement.mac.FP16(m)(0) := oneComplement(io.i.mant.mac.FP16(m)(0),io.i.sign.mac.FP16(m)(0))
+    OneComplement.add.FP16(m)(0) := oneComplement(io.i.mant.add.FP16(m)(0),io.i.sign.add.FP16(m)(0))
   })
   Array.tabulate(2)((m) => {
     Array.tabulate(8)((n) => {
-      sInt.mul.FP32(m)(n) := io.i.mant.mul.FP32(m)(n).twoComplement(io.i.sign.mul.FP32(m)(n))
-      sInt.mul.TF32(m)(n) := io.i.mant.mul.TF32(m)(n).twoComplement(io.i.sign.mul.TF32(m)(n))
+      OneComplement.mul.FP32(m)(n) := oneComplement(io.i.mant.mul.FP32(m)(n),io.i.sign.mul.FP32(m)(n))
+      OneComplement.mul.TF32(m)(n) := oneComplement(io.i.mant.mul.TF32(m)(n),io.i.sign.mul.TF32(m)(n))
     })
-    sInt.mac.FP32(m)(0) := io.i.mant.mac.FP32(m)(0).twoComplement(io.i.sign.mac.FP32(m)(0))
-    sInt.mac.TF32(m)(0) := io.i.mant.mac.TF32(m)(0).twoComplement(io.i.sign.mac.TF32(m)(0))
-    sInt.add.FP32(m)(0) := io.i.mant.add.FP32(m)(0).twoComplement(io.i.sign.add.FP32(m)(0))
-    sInt.add.TF32(m)(0) := io.i.mant.add.TF32(m)(0).twoComplement(io.i.sign.add.TF32(m)(0))
-    io.o.FP32(m) :=(ctl.i.mode.isMUL? sum(sInt.mul.FP32(m)) |
-                   (ctl.i.mode.isMAC? sum(sInt.mul.FP32(m),sInt.mac.FP32(m))| 
-                   (ctl.i.mode.isMAC? sum(sInt.add.FP32(m),sInt.mac.FP32(m))| S"0".resized))).resized
-    io.o.TF32(m) :=(ctl.i.mode.isMUL? sum(sInt.mul.TF32(m)) |
-                   (ctl.i.mode.isMAC? sum(sInt.mul.TF32(m),sInt.mac.TF32(m))| 
-                   (ctl.i.mode.isMAC? sum(sInt.add.TF32(m),sInt.mac.TF32(m))| S"0".resized))).resized
+    OneComplement.mac.FP32(m)(0) := oneComplement(io.i.mant.mac.FP32(m)(0),io.i.sign.mac.FP32(m)(0))
+    OneComplement.mac.TF32(m)(0) := oneComplement(io.i.mant.mac.TF32(m)(0),io.i.sign.mac.TF32(m)(0))
+    OneComplement.add.FP32(m)(0) := oneComplement(io.i.mant.add.FP32(m)(0),io.i.sign.add.FP32(m)(0))
+    OneComplement.add.TF32(m)(0) := oneComplement(io.i.mant.add.TF32(m)(0),io.i.sign.add.TF32(m)(0))
   })
   Array.tabulate(1)((m) => {
     Array.tabulate(4)((n) => {
-      sInt.mul.FP64(m)(n) := io.i.mant.mul.FP64(m)(n).twoComplement(io.i.sign.mul.FP64(m)(n))
+      OneComplement.mul.FP64(m)(n) := oneComplement(io.i.mant.mul.FP64(m)(n),io.i.sign.mul.FP64(m)(n))
     })
-    sInt.mac.FP64(m)(0) := io.i.mant.mac.FP64(m)(0).twoComplement(io.i.sign.mac.FP64(m)(0))
-    sInt.add.FP64(m)(0) := io.i.mant.add.FP64(m)(0).twoComplement(io.i.sign.add.FP64(m)(0))
-    io.o.FP64(m) :=(ctl.i.mode.isMUL? sum(sInt.mul.FP64(m)) |
-                   (ctl.i.mode.isMAC? sum(sInt.mul.FP64(m),sInt.mac.FP64(m))| 
-                   (ctl.i.mode.isMAC? sum(sInt.add.FP64(m),sInt.mac.FP64(m))| S"0".resized))).resized
+    OneComplement.mac.FP64(m)(0) := oneComplement(io.i.mant.mac.FP64(m)(0),io.i.sign.mac.FP64(m)(0))
+    OneComplement.add.FP64(m)(0) := oneComplement(io.i.mant.add.FP64(m)(0),io.i.sign.add.FP64(m)(0))
   })
+
+  // Array.tabulate(4)((m) => {
+  //   io.o.INT8(m) :=(ctl.i.mode.isMUL? sum(OneComplement.mul.INT8(m)) |
+  //                  (ctl.i.mode.isMAC? sum(OneComplement.mul.INT8(m),OneComplement.mac.INT8(m))| 
+  //                  (ctl.i.mode.isMAC? sum(OneComplement.add.INT8(m),OneComplement.mac.INT8(m))| S"0".resized))).resized
+  // })
+  // Array.tabulate(4)((m) => {
+  //   io.o.BF16(m) :=(ctl.i.mode.isMUL? sum(OneComplement.mul.BF16(m)) |
+  //                  (ctl.i.mode.isMAC? sum(OneComplement.mul.BF16(m),OneComplement.mac.BF16(m))| 
+  //                  (ctl.i.mode.isMAC? sum(OneComplement.add.BF16(m),OneComplement.mac.BF16(m))| S"0".resized))).resized
+  //   io.o.FP16(m) :=(ctl.i.mode.isMUL? sum(OneComplement.mul.FP16(m)) |
+  //                  (ctl.i.mode.isMAC? sum(OneComplement.mul.FP16(m),OneComplement.mac.FP16(m))| 
+  //                  (ctl.i.mode.isMAC? sum(OneComplement.add.FP16(m),OneComplement.mac.FP16(m))| S"0".resized))).resized
+  // })
+  // Array.tabulate(2)((m) => {
+  //   io.o.FP32(m) :=(ctl.i.mode.isMUL? sum(OneComplement.mul.FP32(m)) |
+  //                  (ctl.i.mode.isMAC? sum(OneComplement.mul.FP32(m),OneComplement.mac.FP32(m))| 
+  //                  (ctl.i.mode.isMAC? sum(OneComplement.add.FP32(m),OneComplement.mac.FP32(m))| S"0".resized))).resized
+  //   io.o.TF32(m) :=(ctl.i.mode.isMUL? sum(OneComplement.mul.TF32(m)) |
+  //                  (ctl.i.mode.isMAC? sum(OneComplement.mul.TF32(m),OneComplement.mac.TF32(m))| 
+  //                  (ctl.i.mode.isMAC? sum(OneComplement.add.TF32(m),OneComplement.mac.TF32(m))| S"0".resized))).resized
+  // })
+  // Array.tabulate(1)((m) => {
+  //   io.o.FP64(m) :=(ctl.i.mode.isMUL? sum(OneComplement.mul.FP64(m)) |
+  //                  (ctl.i.mode.isMAC? sum(OneComplement.mul.FP64(m),OneComplement.mac.FP64(m))| 
+  //                  (ctl.i.mode.isMAC? sum(OneComplement.add.FP64(m),OneComplement.mac.FP64(m))| S"0".resized))).resized
+  // })
 }
 object ADDMulti_Verilog {
   def main(args: Array[String]): Unit = {
@@ -357,7 +356,7 @@ class ADDMulti_Verif() extends Component{
     val sim = Reg(Bool())
   }
   noIoPrefix()
-  val sInt = new Bundle{
+  val OneComplement = new Bundle{
     val mul = new Bundle{
       val INT8 = Vec(Vec(SInt(  8*2+1 bits), 16), 4)
       val BF16 = Vec(Vec(SInt(  8*2+1 bits), 16), 4)
@@ -385,55 +384,55 @@ class ADDMulti_Verif() extends Component{
   }
   Array.tabulate(4)((m) => {
     Array.tabulate(16)((n) => {
-      sInt.mul.INT8(m)(n) := io.i.mant.mul.INT8(m)(n)
+      OneComplement.mul.INT8(m)(n) := io.i.mant.mul.INT8(m)(n)
     })
-    sInt.mac.INT8(m)(0) := io.i.mant.mac.INT8(m)(0)
-    sInt.add.INT8(m)(0) := io.i.mant.add.INT8(m)(0)
-    verify.o.INT8(m) :=(ctl.i.mode.isMUL? sum(sInt.mul.INT8(m)) |
-                   (ctl.i.mode.isMAC? sum(sInt.mul.INT8(m),sInt.mac.INT8(m))| 
-                   (ctl.i.mode.isMAC? sum(sInt.add.INT8(m),sInt.mac.INT8(m))| S"0".resized))).resized
+    OneComplement.mac.INT8(m)(0) := io.i.mant.mac.INT8(m)(0)
+    OneComplement.add.INT8(m)(0) := io.i.mant.add.INT8(m)(0)
+    verify.o.INT8(m) :=(ctl.i.mode.isMUL? sum(OneComplement.mul.INT8(m)) |
+                   (ctl.i.mode.isMAC? sum(OneComplement.mul.INT8(m),OneComplement.mac.INT8(m))| 
+                   (ctl.i.mode.isMAC? sum(OneComplement.add.INT8(m),OneComplement.mac.INT8(m))| S"0".resized))).resized
   })
   Array.tabulate(4)((m) => {
     Array.tabulate(16)((n) => {
-      sInt.mul.BF16(m)(n) := io.i.mant.mul.BF16(m)(n).twoComplement(io.i.sign.mul.BF16(m)(n))
-      sInt.mul.FP16(m)(n) := io.i.mant.mul.FP16(m)(n).twoComplement(io.i.sign.mul.FP16(m)(n))
+      OneComplement.mul.BF16(m)(n) := io.i.mant.mul.BF16(m)(n).twoComplement(io.i.sign.mul.BF16(m)(n))
+      OneComplement.mul.FP16(m)(n) := io.i.mant.mul.FP16(m)(n).twoComplement(io.i.sign.mul.FP16(m)(n))
     })
-    sInt.mac.BF16(m)(0) := io.i.mant.mac.BF16(m)(0).twoComplement(io.i.sign.mac.BF16(m)(0))
-    sInt.add.BF16(m)(0) := io.i.mant.add.BF16(m)(0).twoComplement(io.i.sign.add.BF16(m)(0))
-    sInt.mac.FP16(m)(0) := io.i.mant.mac.FP16(m)(0).twoComplement(io.i.sign.mac.FP16(m)(0))
-    sInt.add.FP16(m)(0) := io.i.mant.add.FP16(m)(0).twoComplement(io.i.sign.add.FP16(m)(0))
-    verify.o.BF16(m) :=(ctl.i.mode.isMUL? sum(sInt.mul.BF16(m)) |
-                   (ctl.i.mode.isMAC? sum(sInt.mul.BF16(m),sInt.mac.BF16(m))| 
-                   (ctl.i.mode.isMAC? sum(sInt.add.BF16(m),sInt.mac.BF16(m))| S"0".resized))).resized
-    verify.o.FP16(m) :=(ctl.i.mode.isMUL? sum(sInt.mul.FP16(m)) |
-                   (ctl.i.mode.isMAC? sum(sInt.mul.FP16(m),sInt.mac.FP16(m))| 
-                   (ctl.i.mode.isMAC? sum(sInt.add.FP16(m),sInt.mac.FP16(m))| S"0".resized))).resized
+    OneComplement.mac.BF16(m)(0) := io.i.mant.mac.BF16(m)(0).twoComplement(io.i.sign.mac.BF16(m)(0))
+    OneComplement.add.BF16(m)(0) := io.i.mant.add.BF16(m)(0).twoComplement(io.i.sign.add.BF16(m)(0))
+    OneComplement.mac.FP16(m)(0) := io.i.mant.mac.FP16(m)(0).twoComplement(io.i.sign.mac.FP16(m)(0))
+    OneComplement.add.FP16(m)(0) := io.i.mant.add.FP16(m)(0).twoComplement(io.i.sign.add.FP16(m)(0))
+    verify.o.BF16(m) :=(ctl.i.mode.isMUL? sum(OneComplement.mul.BF16(m)) |
+                   (ctl.i.mode.isMAC? sum(OneComplement.mul.BF16(m),OneComplement.mac.BF16(m))| 
+                   (ctl.i.mode.isMAC? sum(OneComplement.add.BF16(m),OneComplement.mac.BF16(m))| S"0".resized))).resized
+    verify.o.FP16(m) :=(ctl.i.mode.isMUL? sum(OneComplement.mul.FP16(m)) |
+                   (ctl.i.mode.isMAC? sum(OneComplement.mul.FP16(m),OneComplement.mac.FP16(m))| 
+                   (ctl.i.mode.isMAC? sum(OneComplement.add.FP16(m),OneComplement.mac.FP16(m))| S"0".resized))).resized
   })
   Array.tabulate(2)((m) => {
     Array.tabulate(8)((n) => {
-      sInt.mul.FP32(m)(n) := io.i.mant.mul.FP32(m)(n).twoComplement(io.i.sign.mul.FP32(m)(n))
-      sInt.mul.TF32(m)(n) := io.i.mant.mul.TF32(m)(n).twoComplement(io.i.sign.mul.TF32(m)(n))
+      OneComplement.mul.FP32(m)(n) := io.i.mant.mul.FP32(m)(n).twoComplement(io.i.sign.mul.FP32(m)(n))
+      OneComplement.mul.TF32(m)(n) := io.i.mant.mul.TF32(m)(n).twoComplement(io.i.sign.mul.TF32(m)(n))
     })
-    sInt.mac.FP32(m)(0) := io.i.mant.mac.FP32(m)(0).twoComplement(io.i.sign.mac.FP32(m)(0))
-    sInt.mac.TF32(m)(0) := io.i.mant.mac.TF32(m)(0).twoComplement(io.i.sign.mac.TF32(m)(0))
-    sInt.add.FP32(m)(0) := io.i.mant.add.FP32(m)(0).twoComplement(io.i.sign.add.FP32(m)(0))
-    sInt.add.TF32(m)(0) := io.i.mant.add.TF32(m)(0).twoComplement(io.i.sign.add.TF32(m)(0))
-    verify.o.FP32(m) :=(ctl.i.mode.isMUL? sum(sInt.mul.FP32(m)) |
-                   (ctl.i.mode.isMAC? sum(sInt.mul.FP32(m),sInt.mac.FP32(m))| 
-                   (ctl.i.mode.isMAC? sum(sInt.add.FP32(m),sInt.mac.FP32(m))| S"0".resized))).resized
-    verify.o.TF32(m) :=(ctl.i.mode.isMUL? sum(sInt.mul.TF32(m)) |
-                   (ctl.i.mode.isMAC? sum(sInt.mul.TF32(m),sInt.mac.TF32(m))| 
-                   (ctl.i.mode.isMAC? sum(sInt.add.TF32(m),sInt.mac.TF32(m))| S"0".resized))).resized
+    OneComplement.mac.FP32(m)(0) := io.i.mant.mac.FP32(m)(0).twoComplement(io.i.sign.mac.FP32(m)(0))
+    OneComplement.mac.TF32(m)(0) := io.i.mant.mac.TF32(m)(0).twoComplement(io.i.sign.mac.TF32(m)(0))
+    OneComplement.add.FP32(m)(0) := io.i.mant.add.FP32(m)(0).twoComplement(io.i.sign.add.FP32(m)(0))
+    OneComplement.add.TF32(m)(0) := io.i.mant.add.TF32(m)(0).twoComplement(io.i.sign.add.TF32(m)(0))
+    verify.o.FP32(m) :=(ctl.i.mode.isMUL? sum(OneComplement.mul.FP32(m)) |
+                   (ctl.i.mode.isMAC? sum(OneComplement.mul.FP32(m),OneComplement.mac.FP32(m))| 
+                   (ctl.i.mode.isMAC? sum(OneComplement.add.FP32(m),OneComplement.mac.FP32(m))| S"0".resized))).resized
+    verify.o.TF32(m) :=(ctl.i.mode.isMUL? sum(OneComplement.mul.TF32(m)) |
+                   (ctl.i.mode.isMAC? sum(OneComplement.mul.TF32(m),OneComplement.mac.TF32(m))| 
+                   (ctl.i.mode.isMAC? sum(OneComplement.add.TF32(m),OneComplement.mac.TF32(m))| S"0".resized))).resized
   })
   Array.tabulate(1)((m) => {
     Array.tabulate(4)((n) => {
-      sInt.mul.FP64(m)(n) := io.i.mant.mul.FP64(m)(n).twoComplement(io.i.sign.mul.FP64(m)(n))
+      OneComplement.mul.FP64(m)(n) := io.i.mant.mul.FP64(m)(n).twoComplement(io.i.sign.mul.FP64(m)(n))
     })
-    sInt.mac.FP64(m)(0) := io.i.mant.mac.FP64(m)(0).twoComplement(io.i.sign.mac.FP64(m)(0))
-    sInt.add.FP64(m)(0) := io.i.mant.add.FP64(m)(0).twoComplement(io.i.sign.add.FP64(m)(0))
-    verify.o.FP64(m) :=(ctl.i.mode.isMUL? sum(sInt.mul.FP64(m)) |
-                   (ctl.i.mode.isMAC? sum(sInt.mul.FP64(m),sInt.mac.FP64(m))| 
-                   (ctl.i.mode.isMAC? sum(sInt.add.FP64(m),sInt.mac.FP64(m))| S"0".resized))).resized
+    OneComplement.mac.FP64(m)(0) := io.i.mant.mac.FP64(m)(0).twoComplement(io.i.sign.mac.FP64(m)(0))
+    OneComplement.add.FP64(m)(0) := io.i.mant.add.FP64(m)(0).twoComplement(io.i.sign.add.FP64(m)(0))
+    verify.o.FP64(m) :=(ctl.i.mode.isMUL? sum(OneComplement.mul.FP64(m)) |
+                   (ctl.i.mode.isMAC? sum(OneComplement.mul.FP64(m),OneComplement.mac.FP64(m))| 
+                   (ctl.i.mode.isMAC? sum(OneComplement.add.FP64(m),OneComplement.mac.FP64(m))| S"0".resized))).resized
   })
     verify.equal.mul.BF16 := verify.o.BF16===io.o.BF16
     verify.equal.mul.FP16 := verify.o.FP16===io.o.FP16
